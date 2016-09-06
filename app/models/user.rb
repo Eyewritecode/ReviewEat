dd6151d4b5a54767c61cc3266ef3104f39cfda43
Eyewritecode <933kss@gmail.com>
2016-09-06 23:36:08 +0900

    Reviews to users

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,4 +3,6 @@ class User < ActiveRecord::Base
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+ 	has_many :reviews, dependent: :destroy #if a user is deleted all their reviews are gone too
 end
